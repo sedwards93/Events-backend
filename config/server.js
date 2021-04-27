9d6dd206c7a1e42713bdf5cc98e5fa56c0150040
Symion Edwards <symion.edwards@gmail.com>
2021-04-27 10:03:03 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '25222d44e84fe1696580d5c422079b42'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2817062e-0394-4e79-afb0-0c71091c7834'
+};
